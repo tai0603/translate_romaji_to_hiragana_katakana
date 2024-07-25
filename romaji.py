8cc7e5131f724051e7afce138321e95618f8772c
tai0603 <arestai0603@gmail.com>
2024-07-25 05:10:01 -0400

    v0.0

---
+++ romaji.py
@@ -0,0 +1,112 @@
+# Romaji to Hiragana dictionary
+romaji_to_hiragana = {
+	'a': 'あ', 'i': 'い', 'u': 'う', 'e': 'え', 'o': 'お',
+	'ka': 'か', 'ki': 'き', 'ku': 'く', 'ke': 'け', 'ko': 'こ',
+	'sa': 'さ', 'shi': 'し', 'su': 'す', 'se': 'せ', 'so': 'そ',
+	'ta': 'た', 'chi': 'ち', 'tsu': 'つ', 'te': 'て', 'to': 'と',
+	'na': 'な', 'ni': 'に', 'nu': 'ぬ', 'ne': 'ね', 'no': 'の',
+	'ha': 'は', 'hi': 'ひ', 'fu': 'ふ', 'he': 'へ', 'ho': 'ほ',
+	'ma': 'ま', 'mi': 'み', 'mu': 'む', 'me': 'め', 'mo': 'も',
+	'ya': 'や', 'yu': 'ゆ', 'yo': 'よ',
+	'ra': 'ら', 'ri': 'り', 'ru': 'る', 're': 'れ', 'ro': 'ろ',
+	'wa': 'わ', 'wo': 'を', 'n': 'ん',
+	'ga': 'が', 'gi': 'ぎ', 'gu': 'ぐ', 'ge': 'げ', 'go': 'ご',
+	'za': 'ざ', 'ji': 'じ', 'zu': 'ず', 'ze': 'ぜ', 'zo': 'ぞ',
+	'da': 'だ', 'di': 'ぢ', 'du': 'づ', 'de': 'で', 'do': 'ど',
+	'ba': 'ば', 'bi': 'び', 'bu': 'ぶ', 'be': 'べ', 'bo': 'ぼ',
+	'pa': 'ぱ', 'pi': 'ぴ', 'pu': 'ぷ', 'pe': 'ぺ', 'po': 'ぽ',
+	'kya': 'きゃ', 'kyu': 'きゅ', 'kyo': 'きょ',
+	'sha': 'しゃ', 'shu': 'しゅ', 'sho': 'しょ',
+	'cha': 'ちゃ', 'chu': 'ちゅ', 'cho': 'ちょ',
+	'nya': 'にゃ', 'nyu': 'にゅ', 'nyo': 'にょ',
+	'hya': 'ひゃ', 'hyu': 'ひゅ', 'hyo': 'ひょ',
+	'mya': 'みゃ', 'myu': 'みゅ', 'myo': 'みょ',
+	'rya': 'りゃ', 'ryu': 'りゅ', 'ryo': 'りょ',
+	'gya': 'ぎゃ', 'gyu': 'ぎゅ', 'gyo': 'ぎょ',
+	'ja': 'じゃ', 'ju': 'じゅ', 'jo': 'じょ',
+	'bya': 'びゃ', 'byu': 'びゅ', 'byo': 'びょ',
+	'pya': 'ぴゃ', 'pyu': 'ぴゅ', 'pyo': 'ぴょ'
+}
+
+# Romaji to Katakana dictionary
+romaji_to_katakana = {
+	'a': 'ア', 'i': 'イ', 'u': 'ウ', 'e': 'エ', 'o': 'オ',
+	'ka': 'カ', 'ki': 'キ', 'ku': 'ク', 'ke': 'ケ', 'ko': 'コ',
+	'sa': 'サ', 'shi': 'シ', 'su': 'ス', 'se': 'セ', 'so': 'ソ',
+	'ta': 'タ', 'chi': 'チ', 'tsu': 'ツ', 'te': 'テ', 'to': 'ト',
+	'na': 'ナ', 'ni': 'ニ', 'nu': 'ヌ', 'ne': 'ネ', 'no': 'ノ',
+	'ha': 'ハ', 'hi': 'ヒ', 'fu': 'フ', 'he': 'ヘ', 'ho': 'ホ',
+	'ma': 'マ', 'mi': 'ミ', 'mu': 'ム', 'me': 'メ', 'mo': 'モ',
+	'ya': 'ヤ', 'yu': 'ユ', 'yo': 'ヨ',
+	'ra': 'ラ', 'ri': 'リ', 'ru': 'ル', 're': 'レ', 'ro': 'ロ',
+	'wa': 'ワ', 'wo': 'ヲ', 'n': 'ン',
+	'ga': 'ガ', 'gi': 'ギ', 'gu': 'グ', 'ge': 'ゲ', 'go': 'ゴ',
+	'za': 'ザ', 'ji': 'ジ', 'zu': 'ズ', 'ze': 'ゼ', 'zo': 'ゾ',
+	'da': 'ダ', 'di': 'ヂ', 'du': 'ヅ', 'de': 'デ', 'do': 'ド',
+	'ba': 'バ', 'bi': 'ビ', 'bu': 'ブ', 'be': 'ベ', 'bo': 'ボ',
+	'pa': 'パ', 'pi': 'ピ', 'pu': 'プ', 'pe': 'ペ', 'po': 'ポ',
+	'kya': 'キャ', 'kyu': 'キュ', 'kyo': 'キョ',
+	'sha': 'シャ', 'shu': 'シュ', 'sho': 'ショ',
+	'cha': 'チャ', 'chu': 'チュ', 'cho': 'チョ',
+	'nya': 'ニャ', 'nyu': 'ニュ', 'nyo': 'ニョ',
+	'hya': 'ヒャ', 'hyu': 'ヒュ', 'hyo': 'ヒョ',
+	'mya': 'ミャ', 'myu': 'ミュ', 'myo': 'ミョ',
+	'rya': 'リャ', 'ryu': 'リュ', 'ryo': 'リョ',
+	'gya': 'ギャ', 'gyu': 'ギュ', 'gyo': 'ギョ',
+	'ja': 'ジャ', 'ju': 'ジュ', 'jo': 'ジョ',
+	'bya': 'ビャ', 'byu': 'ビュ', 'byo': 'ビョ',
+	'pya': 'ピャ', 'pyu': 'ピュ', 'pyo': 'ピョ'
+}
+
+# Function to translate Romaji to Hiragana
+def translate_romaji_to_hiragana(input_string):
+	words = input_string.split()
+	hiragana = [romaji_to_hiragana[word] if word in romaji_to_hiragana else word for word in words]
+	return ''.join(hiragana)
+
+# Function to translate Romaji to Katakana
+def translate_romaji_to_katakana(input_string):
+	words = input_string.split()
+	katakana = [romaji_to_katakana[word] if word in romaji_to_hiragana else word for word in words]
+	return ''.join(katakana)
+
+def translate_romaji_to_hiragana_katakana(input_string):
+	words = input_string.split()
+	hiragana_katakana = []
+	for word in words:
+		if word[0] != '*':
+			if word in romaji_to_hiragana:
+				hiragana_katakana.append(romaji_to_hiragana[word])
+			elif word == '.':
+				hiragana_katakana.append('っ')
+			else:
+				hiragana_katakana.append(word)
+		elif word[0] == '*':
+			word = word.replace('*', '')
+			if word in romaji_to_katakana:
+				hiragana_katakana.append(romaji_to_katakana[word])
+			elif word == '-':
+				hiragana_katakana.append('ー')
+			elif word == '.':
+				hiragana_katakana.append('ッ')
+			else:
+				hiragana_katakana.append(word)
+	return ''.join(hiragana_katakana)
+
+def main():
+	while True:
+		romaji = input("Romaji: ")
+
+		# Translate the input string to Hiragana and Katakana
+		#hiragana_output = translate_romaji_to_hiragana(romaji)
+		#katakana_output = translate_romaji_to_katakana(romaji)
+		hiragana_katakana_output = translate_romaji_to_hiragana_katakana(romaji)
+
+		#print("Hiragana:", hiragana_output)
+		#print("Katakana:", katakana_output)
+		print("Result: ", hiragana_katakana_output)
+		print("================================================================")
+
+if __name__ == "__main__":
+	main()
+
